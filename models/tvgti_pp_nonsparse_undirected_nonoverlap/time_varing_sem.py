db5d1e47c65273d3133220bc2299ffb00ed0016c
Fuma Ito <fmjp1012@gmail.com>
2025-01-29 12:30:45 +0900

    Update model

--- models/tvgti_pp_nonsparse_undirected_nonoverlap/time_varing_sem.py
+++ models/tvgti_pp_nonsparse_undirected_nonoverlap/time_varing_sem.py
@@ -5,9 +5,8 @@ from tqdm import tqdm
 from utils import project_to_zero_diagonal_symmetric
 
 class TimeVaryingSEM:
-    def __init__(self, N, S_0, r, q, rho):
+    def __init__(self, N, S_0, r, q, rho, mu_lambda, show_progress=True, name = "pp_nonsparse_nonoverlap"):
         self.N = N
-
         self.l = N * (N - 1) // 2
         self.S = S_0
 
@@ -17,23 +16,29 @@ class TimeVaryingSEM:
         self.rho = rho
         self.w = 1 / q
 
+        self.mu_lambda = mu_lambda
+        assert self.mu_lambda > 0 and self.mu_lambda < 2
+
+        # tqdmの表示／非表示を制御するフラグ
+        self.show_progress = show_progress
+        self.name = name
+
     def g_l(self, x): # per processor
         return norm(x - self.S @ x) ** 2 / 2 - self.rho
 
     def subgrad_projection(self, x):
         if self.g_l(x) > 0:
             subgrad = self.S @ x @ x.T - x @ x.T
-            return self.S - (self.g_l(x) / (norm(subgrad) ** 2)) * subgrad 
+            return self.S - (self.g_l(x) / (norm(subgrad) ** 2)) * subgrad
         else:
             return self.S
-    
+
     def parallel_projection(self, X_partial):
         in_all_C_l = True
         sum_weighted_projection_sp = np.zeros((self.N, self.N))
 
         numerator = 0.0
         denominator = 0.0
-
 
         for i in range(self.q):
             x_per_processor = X_partial[:, i * self.q: i * self.q + self.r]
@@ -47,15 +52,11 @@ class TimeVaryingSEM:
             if self.g_l(x_per_processor) > 0:
                 in_all_C_l = False
 
-        # print("in_all_C_l: " + str(in_all_C_l))
-        # print("sum_weighted_projection_sp: " + str(norm(sum_weighted_projection_sp)))
         if not in_all_C_l:
             assert numerator > 0
             denominator = norm(sum_weighted_projection_sp - self.S) ** 2
             M_k = numerator / denominator
-            # print("M_k: " + str(M_k))
-
-            self.S = self.S + M_k * (sum_weighted_projection_sp - self.S)
+            self.S = self.S + self.mu_lambda * M_k * (sum_weighted_projection_sp - self.S)
             np.fill_diagonal(self.S, 0)  # Ensure diagonal elements are zero
         else:
             M_k = 1
@@ -63,16 +64,19 @@ class TimeVaryingSEM:
 
     def run(self, X):
         estimates = []
-        for t, x in enumerate(tqdm(X.T, desc="pp_nonsparse")):
-            # print("start------------------------------")
-            # print("t: " + str(t))
+        
+        # tqdmを使うか通常のイテレータにするかをフラグで切り替え
+        if self.show_progress:
+            iterator = tqdm(X.T, desc=self.name)
+        else:
+            iterator = X.T
+
+        for t, x in enumerate(iterator):
             if t - self.q * self.r >= 0:
                 self.parallel_projection(X[:, t - self.q * self.r: t])
             else:
                 self.parallel_projection(X[:, :t+1])
-            
-            # print("norm of S: " + str(norm(self.S)))
-            # print("end------------------------------")
+
             estimates.append(self.S.copy())
         
         return estimates
